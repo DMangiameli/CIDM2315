734da84a19eefd1f6d9ee6e1f01064ad51499c69
DMangiameli <156851275+DMangiameli@users.noreply.github.com>
2024-01-21 18:27:09 -0600

    Upload Homework1

---
+++ Homework1.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float x = 2.5f;
+        float y = 3.3f;
+
+        float z = ((4*x*x) + (3*y));
+        Console.WriteLine(z);
+    }
+}
+
