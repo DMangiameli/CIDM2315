254015591f8b555985dadb7e4f461ec41d4b2ba7
DMangiameli <156851275+DMangiameli@users.noreply.github.com>
2024-02-05 14:11:13 -0600

    Upload Homework3 in 4 Separate Parts
    Q1 (3 points): Write a C# program that:
    - takes as input a number N, if the number N is a prime number, print “N is prime”,
    else print “N is non-prime”.
    
    Q2 (2 points): Write a C# program:
    - input an integer N
    - and it prints on screen the following square pattern, where the number of
    rows and columns is equal to N
    - e.g. for N=5, the following pattern should be printed on the screen:
    
    Q3(5 points): Write a C# program
    - input an integer number N
    - and it prints on screen the following square pattern, where the number of
    rows is equal to N
    - e.g. for N=5, the following pattern should be printed on the screen:
    
    Bonus (5 points): Write a C# program
    - input a number N
    - and it prints on screen the following square pattern, where the number of
    rows is equal to N
    - e.g. for N=5, the following pattern should be printed on screen:

---
+++ Homework3Q3.cs
@@ -0,0 +1,29 @@
+﻿namespace Homework3Q3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter an integer: ");
+
+        if (int.TryParse(Console.ReadLine(), out int N) && N > 0)
+        {
+            // Outputs correct number of rows
+            for (int i = 0; i < N; i++)
+            {
+                // Outputs correct number of columns
+                for (int j = 0; j <= i; j++)
+                {
+                        Console.Write("*");
+                }
+                // After completing row, move to new line
+                Console.WriteLine(); 
+            }
+        }
+        // Considers invalid entries like decimals and negatives
+        else
+        {
+            Console.WriteLine("Invalid entry. Please enter a positive integer.");
+        }
+    }
+}
