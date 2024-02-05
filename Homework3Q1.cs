9d3b9f0a6acce5769161928f37d3e2cf377057dd
DMangiameli <156851275+DMangiameli@users.noreply.github.com>
2024-02-05 14:03:31 -0600

    Uploaded Homework3 in 4 Separate Parts
    Homework 3 - Question 1

---
+++ Homework3Q1.cs
@@ -0,0 +1,42 @@
+﻿namespace Homework3Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer: ");
+
+            // Prompts user input as variable n
+            if (int.TryParse(Console.ReadLine(), out int n))
+            {
+                // Executes IsPrime if the user entry is valid
+                IsPrime(n);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+
+        // Checks whether the input is prime or not, then outputs accordingly
+        static void IsPrime(int number)
+        {
+            if (number <= 1)
+            {
+                Console.WriteLine($"{number} is non-prime.");
+                return;
+            }
+
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine($"{number} is non-prime.");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{number} is prime.");
+        }
+    }
+}
